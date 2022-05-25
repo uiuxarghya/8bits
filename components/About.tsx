70eaed137f8b4373e73d2d569c52ef650c6a64b3
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2022-05-26 04:57:58 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -1,35 +1,47 @@
-import { InfoCircleOutlined } from '@ant-design/icons';
-import { Button, Modal } from 'antd';
-import React, { useState } from 'react';
+import { InfoCircleOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
+import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
 
-
 const About = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-    };
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
-    return (
-        <>
-            <Button type="primary" onClick={showModal} className={styles.aboutBtn}>
-                <InfoCircleOutlined />About
-            </Button>
-            <Modal title="About" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <a href="/">8bits Link Shortner</a> is a simple serverless link shortner made with
-                <a href="https://nextjs.org" > NextJS</a>, <a href="https://ant.design" >Ant Design</a>, <a href="https://vercel.com" >Vercel</a> and <a href="https://www.mongodb.com/atlas" >MongoDB Atlas</a>.
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Button type="primary" onClick={showModal} className={styles.aboutBtn}>
+        <InfoCircleOutlined />
+        About
+      </Button>
+      <Modal
+        title="About"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <a href="https://8bits.vercel.app">8bits Link Shortner</a> is a simple
+        serverless link shortner made by (
+        <a href="https://twitter.com/uiuxarghya">uiuxarghya</a>) aka{" "}
+        <a href="https://uiuxarghya.vercel.app"> Arghya Ghosh</a>, with
+        <a href="https://nextjs.org"> NextJS</a>,{" "}
+        <a href="https://www.typescriptlang.org"> TypeScript</a>,{" "}
+        <a href="https://ant.design">Ant Design</a>,{" "}
+        <a href="https://vercel.com">Vercel</a> and{" "}
+        <a href="https://www.mongodb.com/atlas">MongoDB Atlas</a>.
+      </Modal>
+    </>
+  );
 };
 
 export default About;
