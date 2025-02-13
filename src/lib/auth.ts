5d050ec742beb330f686e205fe64e30c8ac27f93
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-02-13 22:55:08 +0530

    feat(auth): implement authentication using better-auth with MongoDB integration

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import prisma from "@/lib/db";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  appName: "8bits",
+  database: prismaAdapter(prisma, {
+    provider: "mongodb",
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [nextCookies()],
+});
