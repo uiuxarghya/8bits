59cd73741f033d27fe807d332fcb5a596e16874e
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-04-28 17:46:28 +0530

    feat(nav): enhance navigation bar with scroll effect and beta badge

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -3,17 +3,42 @@
 import Link from "next/link";
 
 import Logo from "@/components/shared/logo";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function Nav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsScrolled(window.scrollY > 10);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="sticky inset-x-0 top-0 z-30 w-full transition-all">
+      <div
+        className={`absolute inset-0 block transition-all ${
+          isScrolled
+            ? "border-b border-slate-100 bg-white/75 backdrop-blur-lg dark:border-white/10 dark:bg-black/75"
+            : "border-b border-transparent bg-transparent"
+        }`}
+      ></div>
       <div className="relative mx-auto w-full max-w-screen-lg px-3 lg:px-4 xl:px-0">
         <div className="flex h-14 items-center justify-between">
           <div className="grow basis-0">
             <Link className="block w-fit py-2 pr-2" href="/home">
-              <div className="max-w-fit">
+              <div className="flex max-w-fit items-center gap-2">
                 <Logo className="h-6 w-auto text-black dark:text-white" />
+                <Badge
+                  variant="secondary"
+                  className="font-silkscreen rounded leading-3 tracking-tight"
+                >
+                  beta
+                </Badge>
               </div>
             </Link>
           </div>
