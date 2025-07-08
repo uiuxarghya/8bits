5510ce6ee48bc0caef3757619837ee54ae377b44
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-07-08 05:34:21 +0530

    feat(constants): add GOOGLE_SITE_VERIFICATION and METADATA_BASE_URL constants

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -6,3 +6,11 @@ export const SHORT_LINK_DOMAIN = cleanDomain(
 
 export const GOOGLE_ANALYTICS_ID =
   process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "";
+
+export const GOOGLE_SITE_VERIFICATION =
+  process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "";
+
+export const METADATA_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? new URL("https://" + process.env.VERCEL_URL || "http://localhost:3000")
+    : new URL("http://localhost:3000");
