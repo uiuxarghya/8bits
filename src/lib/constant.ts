ab3e2e1c3eda55f944741af9628fee7899289d4c
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-07-08 05:30:15 +0530

    feat(constants): add GOOGLE_ANALYTICS_ID constant for tracking

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -3,3 +3,6 @@ import { cleanDomain } from "./utils";
 export const SHORT_LINK_DOMAIN = cleanDomain(
   process.env.NEXT_PUBLIC_SHORT_LINK_DOMAIN || "",
 );
+
+export const GOOGLE_ANALYTICS_ID =
+  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "";
